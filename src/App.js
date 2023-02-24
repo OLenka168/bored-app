349b0f3a1eebe1e1485d7f71e642b7e45c1c840d
Elena <olena168@gmail.com>
2023-02-24 21:17:13 +0300

    added https

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   useEffect(() => {
     const getAnswer = async () => {
-      const response = await fetch ('http://www.boredapi.com/api/activity/');
+      const response = await fetch ('https://www.boredapi.com/api/activity/');
       const data = await response.json();
       setAnswer (data.activity);
     }
